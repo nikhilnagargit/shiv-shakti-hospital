f48096d2f7669d92611b54c3e12c340230d05d95
Nikhil Nagar <nikhilnagarcps@gmail.com>
2024-06-05 16:07:29 +0530

    changed project name

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import path from 'path';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,10 +8,10 @@ export default defineConfig({
   resolve: {
     alias: [
       {
-        find: '@',
-        replacement: path.resolve(__dirname, './src'),
+        find: "@",
+        replacement: path.resolve(__dirname, "./src"),
       },
     ],
   },
-  base: '/Hospital-Website',
+  base: "/shiv-shakti-hospital",
 });
