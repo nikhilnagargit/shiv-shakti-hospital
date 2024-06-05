d5320feb5736a041076d0ef80309445149f04266
Nikhil Nagar <nikhilnagarcps@gmail.com>
2024-06-05 15:53:45 +0530

    v

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -1,14 +1,14 @@
-import Logo from '@/assets/Images/Logo.png';
-import NavBar from './NavBar';
-import { useState, useEffect } from 'react';
-import { SelectedPage } from '@/Components/Shared/Types';
+import Logo from "@/assets/Images/logo.png";
+import NavBar from "./NavBar";
+import { useState, useEffect } from "react";
+import { SelectedPage } from "@/Components/Shared/Types";
 
 const Header = () => {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
     SelectedPage.Home
   );
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
-  const flexBetween = 'flex items-center justify-between';
+  const flexBetween = "flex items-center justify-between";
 
   useEffect(() => {
     const handleScroll = () => {
@@ -18,14 +18,14 @@ const Header = () => {
       }
       if (window.scrollY !== 0) setIsTopOfPage(false);
     };
-    window.addEventListener('scroll', handleScroll);
-    return () => window.removeEventListener('scroll', handleScroll);
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
     <div
       className={`${flexBetween} ${
-        isTopOfPage ? '' : 'bg-[#84ceff]'
+        isTopOfPage ? "" : "bg-[#84ceff]"
       } transition fixed top-0 z-30 w-full p-5 md:px-16`}
     >
       <img className="w-10 sm:w-20" src={Logo} alt="Logo" />
